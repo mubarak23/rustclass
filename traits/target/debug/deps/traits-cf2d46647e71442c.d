29417f0b19d1f37b985cb377a2d23ba3f891729e
mubarak23 <mubarakaminu340@gmail.com>
2023-09-24 13:33:19 +0100

    understanding trait and its implementation

---
+++ traits/target/debug/deps/traits-cf2d46647e71442c.d
@@ -0,0 +1,5 @@
+/Users/macbookpro/Documents/rustclass/traits/target/debug/deps/traits-cf2d46647e71442c: src/main.rs
+
+/Users/macbookpro/Documents/rustclass/traits/target/debug/deps/traits-cf2d46647e71442c.d: src/main.rs
+
+src/main.rs:
---
+++ traits/target/debug/traits.d
@@ -0,0 +1,1 @@
+/Users/macbookpro/Documents/rustclass/traits/target/debug/traits: /Users/macbookpro/Documents/rustclass/traits/src/main.rs
